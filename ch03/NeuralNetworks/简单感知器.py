70f9ddc959878317f98c2a00a9e94e8d9a652af2
Michael <zyqing601@163.com>
2021-08-17 20:09:21 +0800

    update

--- ch03/NeuralNetworks/简单感知器.py
+++ ch03/NeuralNetworks/简单感知器.py
@@ -1,8 +1,11 @@
 # %%
 import numpy as np
-import TensorPy as tp
+import tensorflow as tf
 import pylab as plt
-
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+v1 = tf.compat.v1
+v1.disable_v2_behavior()
 # %%
 # randn: 返回服从正态分布的随机值
 # 生成一些集中在（-2, -2）的点
@@ -10,30 +13,31 @@ red_points = np.random.randn(50, 2) - 2 * np.ones((50, 2))
 # 生成一些集中在（2， 2）的点
 blue_points = np.random.randn(50, 2) + 2 * np.ones((50, 2))
 # %%
-tp.Graph().as_default()
+# v1.Graph().as_default()
 # %%
-X = tp.placeholder()
+X = v1.placeholder(dtype=tf.float64)
 # %%
-c = tp.placeholder()
+c = v1.placeholder(dtype=tf.float64)
 # %%
 # 权重矩阵
-W = tp.Variable(np.random.randn(2, 2))
+W = v1.Variable(np.random.randn(2, 2))
 # %%
 # 偏置
-b = tp.Variable(np.random.randn(2))
+b = v1.Variable(np.random.randn(2))
 # %%
-# p = tp.sigmoid(tp.add(tp.matmul(X, W), b))
-p = tp.softmax(tp.add(tp.matmul(X, W), b))
+# p = tf.sigmoid(tf.add(tf.matmul(X, W), b))
+p = tf.nn.softmax(tf.add(tf.matmul(X, W), b))
 # %%
-J = tp.negative(tp.reduce_sum(tp.reduce_sum(tp.multiply(c, tp.log(p)), axis=1)))
+J = tf.negative(tf.reduce_sum(tf.reduce_sum(tf.multiply(c, tf.math.log(p)), axis=1)))
 
 # %%
-minimization_op = tp.GradientDescentOptimizer(learning_rate=0.01).minimize(J)
+minimization_op = v1.train.GradientDescentOptimizer(learning_rate=0.01).minimize(J)
 
 # %%
 feed_dict = {X: np.concatenate((blue_points, red_points)), c: [[1, 0]] * len(blue_points) + [[0, 1]] * len(red_points)}
 # %%
-sess = tp.Session()
+sess = v1.Session()
+sess.run(v1.global_variables_initializer())
 # %%
 for step in range(100):
     J_value = sess.run(J, feed_dict=feed_dict)
@@ -57,3 +61,5 @@ x_axis = np.linspace(-4, 4, 100)
 y_axis = -W_value[0][0] / W_value[1][0] * x_axis - b_value[0] / W_value[1][0]
 plt.plot(x_axis, y_axis)
 plt.show()
+
+# %%
